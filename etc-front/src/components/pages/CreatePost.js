d62862d19da37e65a29d08de7b6b43950b1b19a9
AGPWEB <147631236+AGPWEB@users.noreply.github.com>
2023-10-11 19:41:56 +0300

    Update CreatePost.js

--- etc-front/src/components/pages/CreatePost.js
+++ etc-front/src/components/pages/CreatePost.js
@@ -21,7 +21,7 @@ export default function CreatePost() {
     data.set("content", content);
     data.set("file", files[0]);
 
-    const response = await fetch("http://localhost:4000/post", {
+    const response = await fetch("http://process.env.REACT_APP_URL/post", {
       method: "POST",
       body: data,
       credentials: "include",
