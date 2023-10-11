64f2e1085f50118ef74895fe9011d27c662d2e64
AGPWEB <147631236+AGPWEB@users.noreply.github.com>
2023-10-11 19:40:30 +0300

    Update Header.js

--- etc-front/src/Header.js
+++ etc-front/src/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
      const {setUserInfo, userInfo} = useContext(UserContext);
     //const [username, setUsername] = useState(null);
     useEffect(() => {
-      fetch('http://localhost:4000/profile', {
+      fetch('http://process.env.REACT_APP_URL/profile', {
         credentials: 'include',
       }).then(response =>{
           response.json().then(userInfo => {
@@ -16,7 +16,7 @@ export default function Header(){
     }, []);
 
      function logout() {
-      fetch('http://localhost:4000/logout',{
+      fetch('http://process.env.REACT_APP_URL/logout',{
         credentials: 'include',
         method: 'POST'
       });
