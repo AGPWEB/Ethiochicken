7620fd1045175777b0b6d20fe91e28ff5f2d9c27
AGPWEB <147631236+AGPWEB@users.noreply.github.com>
2023-10-13 17:44:13 +0300

    Update Post.js

--- etc-front/src/Post.js
+++ etc-front/src/Post.js
@@ -110,15 +110,7 @@ export default function Post({_id, title, summary, cover, content, createdAt, au
             </h2>
           </Link>
           <p className="info">
-            <time
-              style={{
-                color: 'burgundy',
-                fontFamily: 'Poppins',
-                fontWeight: 'bolder',
-              }}
-            >
-              {format(new Date(createdAt), 'MMM d, yyyy HH:mm')}
-            </time>
+
           </p>
           <p
             className="summary"
