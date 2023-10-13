d830c8e0e7ee8c9b4b9c9813715e2d3251ab0c5d
AGPWEB <147631236+AGPWEB@users.noreply.github.com>
2023-10-13 10:45:09 +0300

    Update JobDetail.js

--- etc-front/src/components/pages/JobDetail.js
+++ etc-front/src/components/pages/JobDetail.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-
+import Footer from './Footer2';
 import { UserContext } from "../../UserContext";
 
 export default function JobDetail() {
