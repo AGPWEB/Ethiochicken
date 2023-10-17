9a9850ee4d911e60cb6da493919b95d59bb770ff
AGPWEB <147631236+AGPWEB@users.noreply.github.com>
2023-10-17 12:36:44 +0300

    Update SignUp.js

--- etc-front/src/components/pages/SignUp.js
+++ etc-front/src/components/pages/SignUp.js
@@ -5,9 +5,11 @@ import MapComponent from './MapComponent';
 import Footer from './Footer2'
 export default function SignUp() {
   return <>
+   <div style={{marginTop:'100px'}}></div>
     <ContactForm />
     <MapComponent />
     <Footer />
     </>
   ;
 }
+
