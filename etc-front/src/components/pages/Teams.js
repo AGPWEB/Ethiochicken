aba0f95a4166af10682b5ea4c343472506b7fea9
AGPWEB <147631236+AGPWEB@users.noreply.github.com>
2023-10-17 12:28:34 +0300

    Update Teams.js

--- etc-front/src/components/pages/Teams.js
+++ etc-front/src/components/pages/Teams.js
@@ -15,7 +15,7 @@ function Teams() {
     <div className='image-collection2'>
        <div className='image7' data-text="">
            <img src={Justin} alt='dayoldchick' className='teams-images'/>
-           <h3 style={{}}>DR. Justin Benade</h3>
+           <h3 style={{}}>Dr. Justin Benade</h3>
            <p>Managing Director</p>
        </div>
        <div className='image8' data-text="">
@@ -32,12 +32,12 @@ function Teams() {
     <div className='image-collection2'>
        <div className='image7' data-text="">
            <img src={DR_Mulualem} alt='dayoldchick' className='teams-images'/>
-           <h3 style={{}}>DR. Mulualem Tesfaye</h3>
+           <h3 style={{}}>Dr. Mulualem Tesfaye</h3>
            <p>Sales and Marketing Director</p>
        </div>
        <div className='image8' data-text="">
            <img src={DR_Fikresilassie} alt='dayoldchick' className='teams-images' />
-           <h3  style={{}}>DR. Fikresilassie Dawit</h3>
+           <h3  style={{}}>Dr. Fikresilassie Dawit</h3>
            <p>Production Director</p>
       </div>
       <div className='image9'  data-text="">
@@ -54,7 +54,7 @@ function Teams() {
        </div>
        <div className='image8' data-text="">
            <img src={Sean} alt='dayoldchick' className='teams-images' />
-           <h3  style={{}}>Mr. Sean Moran</h3>
+           <h3  style={{}}>Ms. Sean Moran</h3>
            <p>Supply Chain and Logistics Director</p>
       </div>
       
